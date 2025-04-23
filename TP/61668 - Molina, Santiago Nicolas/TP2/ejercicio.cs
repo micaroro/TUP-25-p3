df5959d61639795339660943eceda6ecae74fb5d
NicolasMolina634 <nicolasmolina0200@gmail.com>
2025-04-23 17:52:51 -0300

    61668 Santiago Nicolas Molina (#318)
    * Update ejercicio.cs
    
    * Update ejercicio.cs

--- TP/61668 - Molina, Santiago Nicolas/TP2/ejercicio.cs
+++ TP/61668 - Molina, Santiago Nicolas/TP2/ejercicio.cs
@@ -1,60 +1,303 @@
-// TP2: Sistema de Cuentas Bancarias
-//
+using System;
 
-// Implementar un sistema de cuentas bancarias que permita realizar operaciones como depósitos, retiros, transferencias y pagos.
+public class SistemaBancario {
+	/* Clientes */
+		class Cliente {
+			public string Nombre { get => nombre; }
+				protected string nombre;
+			Cuenta[] cuentas;
 
-class Banco{}
-class Cliente{}
+			public Cliente( string nombre = null ){
+				this.nombre = nombre;
+				this.cuentas = new Cuenta[0];
+			}
 
-abstract class Cuenta{}
-class CuentaOro: Cuenta{}
-class CuentaPlata: Cuenta{}
-class CuentaBronce: Cuenta{}
+			public Cuenta Agregar( Cuenta nueva ){
+				Array.Resize( ref cuentas, cuentas.Length + 1 );
+				nueva.AsignarTitular( this );
+				cuentas[ cuentas.Length - 1 ] = nueva;
+			return nueva; }
+			
+			public Cuenta Buscar( long numero ){
+				Cuenta encontrada = null;
+				for( int c = 0; c < cuentas.Length && encontrada == null; ++c )
+					if( cuentas[ c ].Numero == numero ) encontrada = cuentas[ c ];
+			return encontrada; }
 
-abstract class Operacion{}
-class Deposito: Operacion{}
-class Retiro: Operacion{}
-class Transferencia: Operacion{}
-class Pago: Operacion{}
+			public Cuenta Buscar( Cuenta buscada ){
+				Cuenta encontrada = null;
+				for( int c = 0; c < cuentas.Length && encontrada == null; ++c )
+					if( cuentas[ c ] == buscada ) encontrada = cuentas[ c ];
+			return encontrada; }
+
+			public void Informe(){
+				double saldo_total = 0.0;
+				double puntos_total = 0.0;
+				
+				for( int c = 0; c < cuentas.Length; ++c ) saldo_total  += cuentas[ c ].Saldo;
+				for( int c = 0; c < cuentas.Length; ++c ) puntos_total += cuentas[ c ].Puntos;
+
+				Console.WriteLine( "\tCliente: {0} | Saldo Total: {1,9:$#####0.00} | Puntos Total: {2,9:$#####0.00}", nombre, saldo_total, puntos_total );
+				Console.WriteLine();
+				for( int c = 0; c < cuentas.Length; ++c )
+					cuentas[ c ].Informe();
+			}
+		}
+
+	/* Cuentas */
+		abstract class Cuenta {
+			static long numero_siguiente = 10001;
+
+			public long Numero { get => numero; }
+				private long numero;
+			public Cliente Titular { get => titular; }
+				private Cliente titular;
+			public double Saldo { get => saldo; }
+				private double saldo;
+			public double Puntos { get => puntos; }
+				private double puntos;
+			Operacion[] operaciones;
+
+			public Cuenta( double saldo_inicial = 0.0 ) {
+				titular = null;
+				numero = numero_siguiente++;
+				saldo = saldo_inicial;
+				puntos = 0;
+				operaciones = new Operacion[0];
+			}
+			
+			public void AsignarTitular( Cliente titular ){  this.titular = titular;  }
+
+			abstract public double ObtenerPuntos( double monto );
+
+			public double Depositar( double monto ) {
+				saldo += monto;
+			return monto; }
+
+			public double Retirar( double monto ) { /* Devuelve el monto retirado. */
+				monto = Math.Min( monto, saldo );
+				saldo -= monto;
+			return monto; }
+
+			public double Pagar( double monto ) { /* Devuelve el monto pagado. */
+				monto = Retirar( monto );
+				puntos += ObtenerPuntos( monto );
+			return monto; }
+
+			public Operacion Registrar( Operacion operacion ){
+				Array.Resize( ref operaciones, operaciones.Length + 1 );
+				operaciones[ operaciones.Length - 1 ] = operacion;
+			return operacion; }
+
+			public void Informe(){
+				Console.WriteLine( "\t\tCuenta: {0} | Saldo: {1,9:$#####0.00} | Puntos: {2,9:$#####0.00}", numero, saldo, puntos );
+				for( int o = 0; o < operaciones.Length; ++o )
+					operaciones[ o ].Informe( this );
+				Console.WriteLine();
+			}
+		}
+
+		class CuentaOro : Cuenta {
+			public CuentaOro( double saldo_inicial = 0.0 ) : base( saldo_inicial ) {}
+			override public double ObtenerPuntos( double monto ) {  return monto * ( monto >= 1000 ? 0.05 : 0.03 );  }
+		}
+
+		class CuentaPlata : Cuenta {
+			public CuentaPlata( double saldo_inicial = 0.0 ) : base( saldo_inicial ) {}
+			override public double ObtenerPuntos( double monto ) {  return monto * 0.02;  }
+		}
+
+		class CuentaBronce : Cuenta {
+			public CuentaBronce( double saldo_inicial = 0.0 ) : base( saldo_inicial ) {}
+			override public double ObtenerPuntos( double monto ) {  return monto * 0.01;  }
+		}
+
+	/* Operaciones */
+		abstract class Operacion {
+			static long numero_siguiente = 1;
+			static Operacion[] operaciones = new Operacion[0]; /* Registra TODAS las operaciones */
+
+			protected long numero;
+			public Cuenta Cuenta { get => cuenta; }
+				protected Cuenta cuenta;
+			protected double monto;
+
+			protected Operacion(){
+				numero = 0;
+				cuenta = null;
+				monto  = Double.NaN;
+			}
+
+			public Operacion( Cuenta cuenta, double monto ){
+				this.numero = numero_siguiente++;
+				this.cuenta = cuenta;
+				this.monto  = monto;
+
+				Array.Resize( ref operaciones, operaciones.Length + 1 );
+				operaciones[ operaciones.Length - 1 ] = this;
+			}
+
+			public virtual bool Involucra( Cuenta cual ){  return cuenta == cual;  }
+
+			public virtual void Registrar(){
+				cuenta.Registrar( this );
+			}
+
+			public abstract void Operar();
+
+			public abstract void Informe( Cuenta cta );
+			
+			protected abstract void Informe();
+			public static void InformeGlobal() {
+				Console.WriteLine( "Informe Global de Operaciones" );
+				Console.WriteLine( "{0,10}\t{1,10}\t{2,-15}\t{3,11}\t{4}", "Nº Operac.", "Cuenta", "Operación", "Monto", "Otros" );
+				for( int o = 0; o < operaciones.Length; ++o )
+					operaciones[ o ].Informe();
+				Console.WriteLine();
+			}
+		}
+
+		class Deposito : Operacion {
+			public Deposito( Cuenta cuenta, double monto ) : base( cuenta, monto ) {}
+			public override void Operar() {  monto = cuenta.Depositar( monto );  }
+			public override void Informe( Cuenta cta ){
+				Console.WriteLine( "\t\t\t{0,10:D}\t{1,-15}\t{2,11:+$#####0.00}", numero, "Depósito", monto );
+			}
+			protected override void Informe(){
+				Console.WriteLine( "{0,10:D}\t{1,10:D}\t{2,-15}\t{3,11:+$#####0.00}", numero, cuenta.Numero, "Depósito", monto );
+			}
+		}
+
+		class Retiro : Operacion {
+			public Retiro( Cuenta cuenta, double monto ) : base( cuenta, monto ) {}
+			override public void Operar() {  monto = cuenta.Retirar( monto );  }
+			public override void Informe( Cuenta cta ){
+				Console.WriteLine( "\t\t\t{0,10:D}\t{1,-15}\t{2,11:-$#####0.00}", numero, "Retiro", monto );
+			}
+			protected override void Informe(){
+				Console.WriteLine( "{0,10:D}\t{1,10:D}\t{2,-15}\t{3,11:-$#####0.00}", numero, cuenta.Numero, "Retiro", monto );
+			}
+		}
+
+		class Pago : Operacion {
+			public Pago( Cuenta cuenta, double monto ) : base( cuenta, monto ) {}
+			override public void Operar() {  monto = cuenta.Pagar( monto );  }
+			public override void Informe( Cuenta cta ){
+				Console.WriteLine( "\t\t\t{0,10:D}\t{1,-15}\t{2,11:-$#####0.00}", numero, "Pago", monto );
+			}
+			protected override void Informe(){
+				Console.WriteLine( "{0,10:D}\t{1,10:D}\t{2,-15}\t{3,11:-$#####0.00}", numero, cuenta.Numero, "Pago", monto );
+			}
+		}
+
+		class Transferencia : Operacion {
+			Cuenta destino;
+
+			public Transferencia() : base() { destino = null; }
+			public Transferencia( Cuenta cuenta, double monto ) : this() {}
+			public Transferencia( Cuenta cuenta, Cuenta destino, double monto ) : base( cuenta, monto ) {
+				this.destino = destino;
+			}
+
+			public override bool Involucra( Cuenta cual ){  return base.Involucra( cual ) || ( destino == cual );  }
+
+			public override void Registrar(){
+				base.Registrar();
+				destino.Registrar( this );
+			}
+
+			public override void Operar() {
+				if( cuenta != destino ){
+					monto = destino.Depositar( cuenta.Retirar( monto ) );
+				} else {
+					monto = 0;
+				};
+			}
+			
+			public override void Informe( Cuenta informando ){
+				if( informando == cuenta )
+					Console.WriteLine( "\t\t\t{0,10:D}\t{1,-15}\t{2,11:-$#####0.00}\tHacia {3}/{4}", numero, "Transferencia", monto, destino.Numero, destino.Titular.Nombre );
+				else
+					Console.WriteLine( "\t\t\t{0,10:D}\t{1,-15}\t{2,11:+$#####0.00}\tDesde {3}/{4}", numero, "Transferencia", monto, cuenta.Numero,  cuenta.Titular.Nombre  );
+			}
+
+			protected override void Informe(){
+				Console.WriteLine( "{0,10:D}\t{1,10:D}\t{2,-15}\t{3,11:-$#####0.00}\tHacia {4,10:D}", numero, cuenta.Numero, "Transferencia", monto, destino.Numero );
+			}
+		}
+
+	/* Bancos */
+		class Banco {
+			string nombre;
+			Cliente[] clientes;
+
+			public Banco( string nombre = null ){
+				this.nombre = nombre;
+				this.clientes = new Cliente[0];
+			}
+
+			public Cliente Agregar( Cliente nuevo ){
+				Array.Resize( ref clientes, clientes.Length + 1 );
+				clientes[ clientes.Length - 1 ] = nuevo;
+			return nuevo; }
+
+			public Cuenta BuscarCuenta( long numero ){
+				Cuenta encontrada = null;
+				for( int c = 0; c < clientes.Length && encontrada == null; ++c )
+					encontrada = clientes[ c ].Buscar( numero );
+			return encontrada; }
+			
+			public Cuenta BuscarCuenta( Cuenta buscada ){
+				Cuenta encontrada = null;
+				for( int c = 0; c < clientes.Length && encontrada == null; ++c )
+					encontrada = clientes[ c ].Buscar( buscada );
+			return encontrada; }
+			
+			public Operacion Registrar( Operacion operacion ){
+				if( BuscarCuenta( operacion.Cuenta ) != null ){
+					operacion.Registrar();
+					operacion.Operar();
+				};
+			return operacion; }
+
+			public void Informe(){
+				Console.WriteLine( "Banco: {0} | Clientes: {1}", nombre, clientes.Length );
+				Console.WriteLine();
+				for( int c = 0; c < clientes.Length; ++c )
+					clientes[ c ].Informe();
+			}
+		}
 
 
-/// EJEMPLO DE USO ///
+	public static void Main() {
+		var nac = new Banco( "Banco Nac" );
+			var raul = nac.Agregar( new Cliente( "Raúl Perez" ) );
+				raul.Agregar( new CuentaOro( 1000 ) );   /* Cuenta Nº10001 */
+				raul.Agregar( new CuentaPlata( 2000 ) ); /* Cuenta Nº10002 */
+			var sara = nac.Agregar( new Cliente( "Sara López" ) );
+				sara.Agregar( new CuentaPlata( 3000 ) ); /* Cuenta Nº10003 */
+				sara.Agregar( new CuentaPlata( 4000 ) ); /* Cuenta Nº10004 */
+		var tup = new Banco( "Banco TUP" );
+			var luis = tup.Agregar( new Cliente( "Luís Gomez" ) );
+				luis.Agregar( new CuentaBronce( 5000 ) ); /* Cuenta Nº10005 */
 
-// Definiciones 
+		nac.Registrar( new Deposito( nac.BuscarCuenta( 10001 ), 100.00 ) );
+		nac.Registrar( new Retiro( nac.BuscarCuenta( 10002 ), 200.00 ) );
+		nac.Registrar( new Transferencia( nac.BuscarCuenta( 10001 ), nac.BuscarCuenta( 10002 ), 300.00 ) );
+		nac.Registrar( new Transferencia( nac.BuscarCuenta( 10003 ), nac.BuscarCuenta( 10004 ), 500.00 ) );
+		nac.Registrar( new Pago( nac.BuscarCuenta( 10002 ), 400.00 ) );
 
-var raul = new Cliente("Raul Perez");
-    raul.Agregar(new CuentaOro("10001", 1000));
-    raul.Agregar(new CuentaPlata("10002", 2000));
+		tup.Registrar( new Deposito( tup.BuscarCuenta( 10005 ), 100.00 ) );
+		tup.Registrar( new Retiro( tup.BuscarCuenta( 10005 ), 200.00 ) );
+		tup.Registrar( new Transferencia( tup.BuscarCuenta( 10005 ), nac.BuscarCuenta( 10002 ), 300.00 ) );
+		tup.Registrar( new Pago( tup.BuscarCuenta( 10005 ), 400.00 ) );
+		
+		nac.Informe();
+		tup.Informe();
+		
+		Console.WriteLine();
+		Operacion.InformeGlobal();
+	}
+}
 
-var sara = new Cliente("Sara Lopez");
-    sara.Agregar(new CuentaPlata("10003", 3000));
-    sara.Agregar(new CuentaPlata("10004", 4000));
-
-var luis = new Cliente("Luis Gomez");
-    luis.Agregar(new CuentaBronce("10005", 5000));
-
-var nac = new Banco("Banco Nac");
-nac.Agregar(raul);
-nac.Agregar(sara);
-
-var tup = new Banco("Banco TUP");
-tup.Agregar(luis);
-
-
-// Registrar Operaciones
-nac.Registrar(new Deposito("10001", 100));
-nac.Registrar(new Retiro("10002", 200));
-nac.Registrar(new Transferencia("10001", "10002", 300));
-nac.Registrar(new Transferencia("10003", "10004", 500));
-nac.Registrar(new Pago("10002", 400));
-
-tup.Registrar(new Deposito("10005", 100));
-tup.Registrar(new Retiro("10005", 200));
-tup.Registrar(new Transferencia("10005", "10002", 300));
-tup.Registrar(new Pago("10005", 400));
-
-
-// Informe final
-nac.Informe();
-tup.Informe();
 
--- TP/61668 - Molina, Santiago Nicolas/tp1/ejercicio.cs
+++ TP/61668 - Molina, Santiago Nicolas/tp1/ejercicio.cs
@@ -1,17 +1,272 @@
-using System;       // Para usar la consola  (Console)
-using System.IO;    // Para leer archivos    (File)
+using System;
+using System.IO;
 
-// Ayuda: 
-//   Console.Clear() : Borra la pantalla
-//   Console.Write(texto) : Escribe texto sin salto de línea
-//   Console.WriteLine(texto) : Escribe texto con salto de línea
-//   Console.ReadLine() : Lee una línea de texto
-//   Console.ReadKey() : Lee una tecla presionada
+public class Agenda{
+	static bool running = true;
 
-// File.ReadLines(origen) : Lee todas las líneas de un archivo y devuelve una lista de strings
-// File.WriteLines(destino, lineas) : Escribe una lista de líneas en un archivo
+	struct Contacto {
+		public int ID;
+		public String Nombre;
+		public long Telefono;
+		public String Email;
+	};
 
-// Escribir la solucion al TP1 en este archivo. (Borre el ejemplo de abajo)
-Console.WriteLine("Hola, soy el ejercicio 1 del TP1 de la materia Programación 3");
-Console.Write("Presionar una tecla para continuar...");
-Console.ReadKey();
+	const int MAX_CANT_CONTACTOS = 10;
+	static Contacto[] Contactos = new Contacto[ MAX_CANT_CONTACTOS ];
+	static int cant_contactos = 0;
+	static int next_ID = 1;
+	static string csv_path = "agenda.csv";
+
+
+	public static void Pausa(){
+		Console.WriteLine( "Presione cualquier tecla para continuar..." );
+		Console.ReadKey( true );
+		Console.WriteLine();
+	}
+		
+	public static void Agregar_Contacto(){
+		Contacto nuevo = new Contacto();
+		bool aceptado = false;
+
+		Console.WriteLine( "=== Agregar Contacto ===" );
+		if( cant_contactos < MAX_CANT_CONTACTOS ){
+			Console.Write( "Nombre   : " );
+				nuevo.Nombre = Console.ReadLine();
+			do {
+				Console.Write( "Teléfono : " );
+					aceptado = long.TryParse( Console.ReadLine(), out nuevo.Telefono );
+					if( !aceptado )
+						Console.WriteLine( "\t¡Valor incorrecto para teléfono! Ingrese solamente dígitos." );
+			} while( !aceptado );
+			Console.Write( "Email    : " );
+				nuevo.Email = Console.ReadLine();
+				
+			nuevo.ID = next_ID++;
+			Contactos[ cant_contactos++ ] = nuevo;
+			Console.WriteLine( "Contacto agregado con ID = {0}", nuevo.ID );	
+		} else {
+			Console.WriteLine( "No hay lugar para más contactos." );	
+			Console.WriteLine();
+		}
+		
+		Pausa();
+	}
+
+	public static void Modificar_Contacto(){
+		int ID_modificando = 0;
+		int index;
+		string linea;
+		bool aceptado = false;
+
+		Console.WriteLine( "=== Modificar Contacto ===" );
+		if( cant_contactos > 0 ){
+			do {
+				Console.Write( "Ingrese el ID del contacto a modificar: " );
+				aceptado = int.TryParse( Console.ReadLine(), out ID_modificando );
+				if( !aceptado )
+					Console.WriteLine( "\t¡Valor incorrecto para ID! Ingrese solamente dígitos." );
+			} while( !aceptado );
+
+			/* Busca el contacto por ID */
+				for( index = 0; index < cant_contactos && Contactos[ index ].ID != ID_modificando; ++index );
+
+			if( index < cant_contactos ){
+				Console.WriteLine( "Datos actuales => Nombre: {0}, Teléfono : {1}, Email: {2}", Contactos[ index ].Nombre, Contactos[ index ].Telefono, Contactos[ index ].Email );
+				Console.WriteLine( "(Deje el campo en blanco para no modificar)" );
+				Console.WriteLine();
+				Console.Write( "Nombre   : " );
+					linea = Console.ReadLine();
+					if( linea.Length > 0 ) Contactos[ index ].Nombre = linea;
+				do {
+					Console.Write( "Teléfono : " );
+						linea = Console.ReadLine();
+						aceptado = true;
+						if( linea.Length > 0 ){
+							aceptado = long.TryParse( linea, out Contactos[ index ].Telefono );
+							if( !aceptado )
+								Console.WriteLine( "\t¡Valor incorrecto para teléfono! Ingrese solamente dígitos." );
+						};
+				} while( !aceptado );
+				Console.Write( "Email    : " );
+					linea = Console.ReadLine();
+					if( linea.Length > 0 ) Contactos[ index ].Email = linea;
+				Console.WriteLine();
+				Console.WriteLine( "Contacto modificado con éxito." );
+			} else {
+				Console.WriteLine( "No se encontró contacto con ID: {0}", ID_modificando );
+				Console.WriteLine();
+			};
+		} else {
+			Console.WriteLine( "No hay contactos para modificar." );	
+			Console.WriteLine();
+		}
+
+		Pausa();
+	}
+
+	public static void Borrar_Contacto(){
+		int ID_borrando = 0;
+		int index;
+		bool aceptado = false;
+
+		Console.WriteLine( "=== Borrar Contacto ===" );
+		if( cant_contactos > 0 ){
+			do {
+				Console.Write( "Ingrese el ID del contacto a borrar: " );
+				aceptado = int.TryParse( Console.ReadLine(), out ID_borrando );
+				if( !aceptado )
+					Console.WriteLine( "\t¡Valor incorrecto para ID! Ingrese solamente dígitos." );
+			} while( !aceptado );
+
+			/* Busca el contacto por ID */
+				for( index = 0; index < cant_contactos && Contactos[ index ].ID != ID_borrando; ++index );
+
+			if( index < cant_contactos ){
+				--cant_contactos;
+				for( ; index < cant_contactos; ++index )
+					Contactos[ index ] = Contactos[ index + 1 ];
+					
+				Console.WriteLine( "Contacto con ID={0} eliminado con éxito.", ID_borrando );
+			} else {
+				Console.WriteLine( "No se encontró contacto con ID: {0}", ID_borrando );
+				Console.WriteLine();
+			};
+		} else {
+			Console.WriteLine( "No hay contactos para borrar." );	
+			Console.WriteLine();
+		}
+
+		Pausa();
+	}
+
+	public static void Listar_Contactos(){
+		Console.WriteLine( "=== Lista de Contactos ===" );
+		Console.WriteLine( "{0,-5}\t{1,-50}\t{2,-15}\t{3,-50}", "ID", "NOMBRE", "TELÉFONO", "EMAIL" );
+		for( int c = 0; c < cant_contactos; ++c )
+			Console.WriteLine( "{0,-5}\t{1,-50}\t{2,-15}\t{3,-50}"
+			, Contactos[ c ].ID
+			, Contactos[ c ].Nombre.Substring( 0, Math.Min( Contactos[ c ].Nombre.Length, 50 ) )
+			, Contactos[ c ].Telefono
+			, Contactos[ c ].Email.Substring(  0, Math.Min( Contactos[ c ].Email.Length,  50 ) )
+			);
+
+		Pausa();
+	}
+
+	public static void Buscar_Contacto(){
+		string termino;
+
+		Console.WriteLine( "=== Buscar Contacto ===" );
+		if( cant_contactos > 0 ){
+			Console.Write( "Ingrese un término de búsqueda (nombre, teléfono o email): " );
+			termino = Console.ReadLine();
+			if( termino.Length > 0 ){
+				Console.WriteLine();
+				Console.WriteLine( "Resultados de la búsqueda:" );
+				Console.WriteLine( "{0,-5}\t{1,-50}\t{2,-15}\t{3,-50}", "ID", "NOMBRE", "TELÉFONO", "EMAIL" );
+				for( int c = 0; c < cant_contactos; ++c )
+					if( Contactos[ c ].Nombre.IndexOf( termino ) > -1
+					 || Contactos[ c ].Telefono.ToString().IndexOf( termino ) > -1
+					 || Contactos[ c ].Email.IndexOf( termino ) > -1
+					)
+						Console.WriteLine( "{0,-5}\t{1,-50}\t{2,-15}\t{3,-50}"
+						, Contactos[ c ].ID
+						, Contactos[ c ].Nombre.Substring( 0, Math.Min( Contactos[ c ].Nombre.Length, 50 ) )
+						, Contactos[ c ].Telefono
+						, Contactos[ c ].Email.Substring(  0, Math.Min( Contactos[ c ].Email.Length,  50 ) )
+						);
+			} else {
+				Console.WriteLine();
+				Console.WriteLine( "No hay término de búsqueda. Sin resultados." );
+			};
+			Console.WriteLine();
+		} else {
+			Console.WriteLine( "No hay contactos para buscar." );	
+			Console.WriteLine();
+		}
+
+		Pausa();
+	}
+
+	public static void Salir(){
+		Console.WriteLine( "Saliendo de la aplicación..." );
+		running = false;
+	}
+
+
+	public static void Main(){
+		char opcion;
+
+		/* Inicialización */
+			for( int c = 0; c < MAX_CANT_CONTACTOS; ++c )
+				Contactos[ c ] = new Contacto(){ ID = 0, Nombre = "", Telefono = 0, Email = "" };
+
+		/* Lee la agenda desde el archivo. */
+			if( File.Exists( csv_path ) )
+				using( StreamReader reader = new StreamReader( csv_path ) ){
+					string line;
+					
+					reader.ReadLine(); /* Header */
+					while( ( line = reader.ReadLine() ) != null ){
+						int from = 0, pos = 0;
+
+						if(  from < line.Length  &&  ( pos = line.IndexOf( ',', from ) ) > -1  ){
+							if( ! int.TryParse( line.Substring( from, pos - from ), out Contactos[ cant_contactos ].ID ) )
+								Contactos[ cant_contactos ].ID = next_ID++;
+							from = pos + 1;
+						};
+						if(  from < line.Length  &&  ( pos = line.IndexOf( ',', from ) ) > -1  ){
+							Contactos[ cant_contactos ].Nombre = line.Substring( from, pos - from );
+							from = pos + 1;
+						};
+						if(  from < line.Length  &&  ( pos = line.IndexOf( ',', from ) ) > -1  ){
+							if( ! long.TryParse( line.Substring( from, pos - from ), out Contactos[ cant_contactos ].Telefono ) )
+								Contactos[ cant_contactos ].Telefono = 0;
+							from = pos + 1;
+						};
+						if(  from < line.Length  ){
+							Contactos[ cant_contactos ].Email = line.Substring( from );
+						};
+						
+						next_ID = Math.Max( next_ID, Contactos[ cant_contactos ].ID + 1 );
+						++cant_contactos;
+					};
+				};		
+
+		/* Ciclo principal. */			
+			do{
+				Console.WriteLine( "===== AGENDA DE CONTACTOS =====" );
+				Console.WriteLine( "1) Agregar contacto"   );
+				Console.WriteLine( "2) Modificar contacto" );
+				Console.WriteLine( "3) Borrar contacto"    );
+				Console.WriteLine( "4) Listar contactos"   );
+				Console.WriteLine( "5) Buscar contacto"    );
+				Console.WriteLine( "0) Salir"              );
+				Console.WriteLine();
+				Console.Write( "Seleccione una opción: " );
+				 opcion = Convert.ToChar( Console.Read() );
+				 Console.WriteLine();
+				
+				Console.WriteLine();
+				switch( opcion ){
+					case '1' : Agregar_Contacto();   break;
+					case '2' : Modificar_Contacto(); break;
+					case '3' : Borrar_Contacto();    break;
+					case '4' : Listar_Contactos();   break;
+					case '5' : Buscar_Contacto();    break;
+					case '0' : Salir();              break;
+					default :
+						Console.WriteLine( "¡Opción incorrecta!" );
+						Console.WriteLine();
+						break;
+				};
+			} while( running );
+			
+		/* Escribe -o sobrescribe- el archivo de agenda. */ 
+			using( StreamWriter writer = new StreamWriter( csv_path, false ) ){
+				writer.WriteLine( "ID,NOMBRE,TELÉFONO,EMAIL" );
+				for( int c = 0; c < cant_contactos; ++c )
+					writer.WriteLine( "{0},{1},{2},{3}", Contactos[ c ].ID, Contactos[ c ].Nombre, Contactos[ c ].Telefono, Contactos[ c ].Email );
+			};		
+	}
+}
