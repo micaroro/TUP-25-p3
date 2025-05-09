4587cbd782b21667dd5ea0dcf457aabf89b79c19
Rodrigo Federico Paz <149693028+Rodrigopz52@users.noreply.github.com>
2025-05-09 00:37:47 -0300

    TP4 (#341)
    TP4

---
+++ TP/61037 - Paz, Rodrigo Federico/tp4/Migrations/20250507204409_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tp4.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Preguntas",
+                columns: table => new
+                {
+                    PreguntaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Enunciado = table.Column<string>(type: "TEXT", nullable: false),
+                    RespuestaA = table.Column<string>(type: "TEXT", nullable: false),
+                    RespuestaB = table.Column<string>(type: "TEXT", nullable: false),
+                    RespuestaC = table.Column<string>(type: "TEXT", nullable: false),
+                    Correcta = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preguntas", x => x.PreguntaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Preguntas");
+        }
+    }
+}
