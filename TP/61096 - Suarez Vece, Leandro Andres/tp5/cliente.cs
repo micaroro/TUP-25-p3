0f2ee41020d74939b195e15813e950c63d6f6d6b
Leandro Andres Suarez Vece <81536937+leandroVece@users.noreply.github.com>
2025-05-24 18:49:53 -0300

    Tp5-61096 - Suarez  Vece, Leandro Andres (#416)
    Tp5-61096 - Suarez  Vece, Leandro Andres

--- TP/61096 - Suarez Vece, Leandro Andres/tp5/cliente.cs
+++ TP/61096 - Suarez Vece, Leandro Andres/tp5/cliente.cs
@@ -6,26 +6,131 @@ using System.Threading.Tasks;     // Task
 
 var baseUrl = "http://localhost:5000";
 var http = new HttpClient();
-var jsonOpt = new JsonSerializerOptions {
-    PropertyNamingPolicy        = JsonNamingPolicy.CamelCase,
+var jsonOpt = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
     PropertyNameCaseInsensitive = true
 };
 
 // Codigo de ejemplo: Reemplazar por la implementacion real 
 
-async Task<List<Producto>> TraerAsync(){
+async Task<List<Producto>> TraerAsync()
+{
     var json = await http.GetStringAsync($"{baseUrl}/productos");
     return JsonSerializer.Deserialize<List<Producto>>(json, jsonOpt)!;
 }
 
-Console.WriteLine("=== Productos ===");
-foreach (var p in await TraerAsync()) {
-    Console.WriteLine($"{p.Id} {p.Nombre,-20} {p.Precio,15:c}");
+async Task<List<Producto>> TraerListaReponerAsync(int stock)
+{
+    var json = await http.GetStringAsync($"{baseUrl}/productos/{stock}");
+    return JsonSerializer.Deserialize<List<Producto>>(json, jsonOpt)!;
 }
-class Producto {
+
+async Task<List<Producto>> AgregarStockAsync(int id, int stock)
+{
+    var response = await http.PutAsync($"{baseUrl}/productos/agregar/{id}/{stock}", null);
+    if (!response.IsSuccessStatusCode)
+    {
+        throw new Exception($"Error: {response.StatusCode}");
+    }
+
+    var json = await response.Content.ReadAsStringAsync();
+    return JsonSerializer.Deserialize<List<Producto>>(json, jsonOpt)!;
+}
+async Task<List<Producto>> QuitarStockAsync(int id, int stock)
+{
+    var response = await http.PutAsync($"{baseUrl}/productos/quitar/{id}/{stock}", null);
+    if (!response.IsSuccessStatusCode)
+    {
+        throw new Exception($"Error: {response.StatusCode}");
+    }
+
+    var json = await response.Content.ReadAsStringAsync();
+    return JsonSerializer.Deserialize<List<Producto>>(json, jsonOpt)!;
+}
+
+static int LeerEntero(string mensaje)
+{
+    int valor;
+    while (true)
+    {
+        Console.Write(mensaje);
+        if (int.TryParse(Console.ReadLine(), out valor))
+            return valor;
+
+        Console.WriteLine("Por favor ingresa un número válido.");
+    }
+}
+
+int opcion = 0;
+int idPorduct = 0;
+int cantidad = 0;
+do
+{
+    Console.Clear();
+    Console.WriteLine("===== MENÚ =====");
+    Console.WriteLine("1. Listar todos los productos");
+    Console.WriteLine("2. Listar los productos con poco Stock");
+    Console.WriteLine("3. Agregar Stock a un producto");
+    Console.WriteLine("4. Quitar Stock a un producto");
+    Console.WriteLine("0. Salir");
+    Console.Write("Elige una opción: ");
+
+    opcion = LeerEntero("ingrese una opcion: ");
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("=== Productos ===");
+            foreach (var p in await TraerAsync())
+            {
+                Console.WriteLine($"{p.Id} {p.Nombre,-5} {p.Stock,5} {p.Precio,15:c} ");
+            }
+            break;
+        case 2:
+            Console.WriteLine("=== Productos Poco Stock===");
+            foreach (var p in await TraerListaReponerAsync(3))
+            {
+                Console.WriteLine($"{p.Id} {p.Nombre,-5} {p.Stock,5} {p.Precio,15:c} ");
+            }
+            break;
+        case 3:
+            idPorduct = LeerEntero("ingrese el id del producto que desea agregar stock: ");
+            cantidad = LeerEntero("ingrese la cantidad a modificar: ");
+            AgregarStockAsync(idPorduct, cantidad);
+            break;
+        case 4:
+            idPorduct = LeerEntero("ingrese el id del producto que desea quitar stock: ");
+            cantidad = LeerEntero("ingrese la cantidad a modificar: ");
+            var res = QuitarStockAsync(idPorduct, cantidad);
+
+
+            break;
+        case 0:
+            Console.WriteLine("Saliendo...");
+            break;
+        default:
+            Console.WriteLine("Opción no válida, intenta de nuevo.");
+            break;
+    }
+
+    if (opcion != 0)
+    {
+        Console.WriteLine("\nPresiona una tecla para continuar...");
+        Console.ReadKey();
+    }
+
+} while (opcion != 0);
+
+
+
+class Producto
+{
     public int Id { get; set; }
     public string Nombre { get; set; } = null!;
     public decimal Precio { get; set; }
+    public int Stock { get; set; }
+
 }
 
 // Fin del ejemplo
--- TP/61096 - Suarez Vece, Leandro Andres/tp5/servidor.cs
+++ TP/61096 - Suarez Vece, Leandro Andres/tp5/servidor.cs
@@ -2,13 +2,21 @@
 #r "nuget: Microsoft.EntityFrameworkCore, 9.0.4"
 #r "nuget: Microsoft.EntityFrameworkCore.Sqlite, 9.0.4"
 
-using System.Text.Json;                     
+#r "nuget: Microsoft.EntityFrameworkCore, 7.0.11"
+#r "nuget: Microsoft.EntityFrameworkCore.Sqlite, 7.0.11"
+#r "nuget: SQLitePCLRaw.bundle_e_sqlite3, 2.1.4"
+
+
+using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.DependencyInjection;
+
+
+SQLitePCL.Batteries_V2.Init();
 
 //  CONFIGURACIÓN
 var builder = WebApplication.CreateBuilder();
@@ -19,23 +27,72 @@ var app = builder.Build();
 
 app.MapGet("/productos", async (AppDb db) => await db.Productos.ToListAsync());
 
+app.MapGet("/productos/{stock}", async (int stock, AppDb db) => await db.Productos.Where(x => x.Stock <= stock).ToListAsync());
+
+app.MapPut("/productos/agregar/{id}/{stock}", async (int id, int stock, AppDb db) =>
+{
+    var res = await db.Productos.FindAsync(id);
+    res.Stock += stock;
+    await db.SaveChangesAsync();
+    return Results.Ok(res);
+});
+
+app.MapPut("/productos/quitar/{id}/{stock}", async (int id, int stock, AppDb db) =>
+{
+    var res = await db.Productos.FindAsync(id);
+    if (res.Stock < stock)
+        return Results.NotFound($"Producto con ID {id} no encontrado.");
+
+    res.Stock -= stock;
+    await db.SaveChangesAsync();
+    return Results.Ok(res);
+});
+
 var db = app.Services.GetRequiredService<AppDb>();
 db.Database.EnsureCreated(); // crear BD si no existe
 // Agregar productos de ejemplo al crear la base de datos
 
-app.Run("http://localhost:5000"); 
+app.Run("http://localhost:5000");
 // NOTA: Si falla la primera vez, corralo nuevamente.
 
 
 
 // Modelo de datos
-class AppDb : DbContext {
+public class AppDb : DbContext
+{
     public AppDb(DbContextOptions<AppDb> options) : base(options) { }
     public DbSet<Producto> Productos => Set<Producto>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>().HasData(ProductoSeeder.ObtenerProductos());
+    }
 }
 
-class Producto{
+public class Producto
+{
     public int Id { get; set; }
     public string Nombre { get; set; } = null!;
     public decimal Precio { get; set; }
+    public int Stock { get; set; }
+}
+
+public static class ProductoSeeder
+{
+    public static List<Producto> ObtenerProductos()
+    {
+        return new List<Producto>
+        {
+            new Producto { Id = 1, Nombre = "Producto 1", Precio = 100.00m, Stock = 4 },
+            new Producto { Id = 2, Nombre = "Producto 2", Precio = 150.00m, Stock = 4 },
+            new Producto { Id = 3, Nombre = "Producto 3", Precio = 200.00m, Stock = 4 },
+            new Producto { Id = 4, Nombre = "Producto 4", Precio = 250.00m, Stock = 4 },
+            new Producto { Id = 5, Nombre = "Producto 5", Precio = 300.00m, Stock = 3 },
+            new Producto { Id = 6, Nombre = "Producto 6", Precio = 350.00m, Stock = 3 },
+            new Producto { Id = 7, Nombre = "Producto 7", Precio = 400.00m, Stock = 4 },
+            new Producto { Id = 8, Nombre = "Producto 8", Precio = 450.00m, Stock = 2 },
+            new Producto { Id = 9, Nombre = "Producto 9", Precio = 500.00m, Stock = 4 },
+            new Producto { Id = 10, Nombre = "Producto 10", Precio = 550.00m, Stock = 1 }
+        };
+    }
 }
