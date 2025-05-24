8dfbecc6237cba1a6eb0b1744abf3500d53e2649
MikaelPaz12 <mikaelpaz777@gmail.com>
2025-05-24 18:43:04 -0300

    cliente.cs (#478)

--- enunciados/tp5/cliente.cs
+++ enunciados/tp5/cliente.cs
@@ -29,3 +29,42 @@ class Producto {
 }
 
 // Fin del ejemplo
+
+using System.Net.Http.Json;
+
+HttpClient client = new HttpClient { BaseAddress = new Uri("http://localhost:5000") };
+
+while (true)
+{
+    Console.WriteLine("Seleccione una opción:");
+    Console.WriteLine("1. Listar productos\n2. Listar productos a reponer\n3. Agregar stock\n4. Quitar stock\n5. Salir");
+
+    var opcion = Console.ReadLine();
+    if (opcion == "5") break;
+
+    switch (opcion)
+    {
+        case "1":
+            var productos = await client.GetFromJsonAsync<List<Producto>>("/productos");
+            productos?.ForEach(p => Console.WriteLine($"{p.Id} - {p.Nombre} - Stock: {p.Stock}"));
+            break;
+        case "2":
+            var reposicion = await client.GetFromJsonAsync<List<Producto>>("/productos/reposicion");
+            reposicion?.ForEach(p => Console.WriteLine($"{p.Id} - {p.Nombre} - Stock: {p.Stock}"));
+            break;
+        case "3":
+            Console.Write("Ingrese ID del producto: ");
+            var idAdd = Console.ReadLine();
+            Console.Write("Ingrese cantidad a agregar: ");
+            var cantidadAdd = Console.ReadLine();
+            await client.PostAsync($"/productos/agregar/{idAdd}/{cantidadAdd}", null);
+            break;
+        case "4":
+            Console.Write("Ingrese ID del producto: ");
+            var idRemove = Console.ReadLine();
+            Console.Write("Ingrese cantidad a quitar: ");
+            var cantidadRemove = Console.ReadLine();
+            await client.PostAsync($"/productos/quitar/{idRemove}/{cantidadRemove}", null);
+            break;
+    }
+}
