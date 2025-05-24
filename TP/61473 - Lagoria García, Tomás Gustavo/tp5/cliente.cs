f9986592ec441faa073f7f6856346cf12f7f6329
tomaslago27 <116082698+tomaslago27@users.noreply.github.com>
2025-05-24 18:49:14 -0300

    Codeando los .cs de Servidor y Cliente, con su respectiva Prueba (#460)

--- TP/61473 - Lagoria García, Tomás Gustavo/tp5/cliente.cs
+++ TP/61473 - Lagoria García, Tomás Gustavo/tp5/cliente.cs
@@ -19,13 +19,118 @@ async Task<List<Producto>> TraerAsync(){
 }
 
 Console.WriteLine("=== Productos ===");
-foreach (var p in await TraerAsync()) {
+foreach (var p in await TraerAsync())
+{
     Console.WriteLine($"{p.Id} {p.Nombre,-20} {p.Precio,15:c}");
 }
-class Producto {
+
+bool salir = false;
+while (!salir)
+{ Console.Clear();
+    Console.WriteLine("\n--- MENÚ ---");
+    Console.WriteLine("1. Listar productos");
+    Console.WriteLine("2. Listar productos a reponer");
+    Console.WriteLine("3. Agregar stock a un producto");
+    Console.WriteLine("4. Quitar stock a un producto");
+    Console.WriteLine("5. Agregar un producto");
+    Console.WriteLine("6. Salir");
+    Console.Write("Seleccione una opción: ");
+    var opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "1":
+            var json = await http.GetStringAsync($"{baseUrl}/productos");
+            var productos = JsonSerializer.Deserialize<List<Producto>>(json, jsonOpt)!;
+            Console.WriteLine("\nPRODUCTOS DISPONIBLES:");
+            foreach (var p in productos.OrderBy(p => p.Nombre))
+            {
+                Console.WriteLine($"ID:{p.Id}. {p.Nombre,-20} - Precio: {p.Precio:c} - Stock: {p.Cantidad,-5}");
+            }
+            Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.ReadKey();
+            break;
+
+        case "2":
+            var reponerJson = await http.GetStringAsync($"{baseUrl}/productos/reponer");
+            var reponer = JsonSerializer.Deserialize<List<Producto>>(reponerJson, jsonOpt)!;
+            Console.WriteLine("\nPRODUCTOS A REPONER:");
+
+            foreach (var p in reponer.OrderBy(p => p.Nombre))
+            {
+                Console.WriteLine($"ID:{p.Id}. {p.Nombre,-20} - Precio: {p.Precio:c} - Stock: {p.Cantidad,-5}");
+            }
+            Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.ReadKey();
+            break;
+
+        case "3":
+            Console.Write("ID producto: ");
+            var idAgregar = Console.ReadLine();
+            Console.Write("Cantidad a agregar: ");
+            var cantidadAgregar = Console.ReadLine();
+            var resAgregar = await http.PostAsync($"{baseUrl}/productos/{idAgregar}/agregar/{cantidadAgregar}", null);
+            Console.WriteLine(resAgregar.IsSuccessStatusCode ? "Stock agregado." : "Error.");
+            var Cambio = await http.GetStringAsync($"{baseUrl}/productos/porID/{idAgregar}");
+            var CambioStock = JsonSerializer.Deserialize<Producto>(Cambio, jsonOpt)!;
+            Console.WriteLine($"El nuevo stock del producto {CambioStock.Nombre} es {CambioStock.Cantidad}");
+            Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.ReadKey();
+            break;
+
+        case "4":
+            Console.Write("ID producto: ");
+            var idQuitar = Console.ReadLine();
+            Console.Write("Cantidad a quitar: ");
+            var cantidadQuitar = Console.ReadLine();
+            var resQuitar = await http.PostAsync($"{baseUrl}/productos/{idQuitar}/quitar/{cantidadQuitar}", null);
+            Console.WriteLine(resQuitar.IsSuccessStatusCode ? "Stock quitado." : "Error.");
+            var CambioQuitar = await http.GetStringAsync($"{baseUrl}/productos/porID/{idQuitar}");
+            var CambioStockQuitar = JsonSerializer.Deserialize<Producto>(CambioQuitar, jsonOpt)!;
+            Console.WriteLine($"El nuevo stock del producto {CambioStockQuitar.Nombre} es {CambioStockQuitar.Cantidad}");
+            Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.ReadKey();
+            break;
+        case "5":
+            Console.Write("Nombre del producto: ");
+            var NombreP = Console.ReadLine();
+            Console.Write("Precio del Producto: ");
+            var PrecioP = Console.ReadLine();
+            Console.Write("Stock del producto: ");
+            var StockP = Console.ReadLine();
+            var nuevoProducto = new Producto
+            {
+                Nombre = NombreP,
+                Precio = decimal.Parse(PrecioP),
+                Cantidad = int.Parse(StockP)
+            };
+            var nuevo = await http.PostAsync($"{baseUrl}/productos/agregarNuevo", 
+                new StringContent(JsonSerializer.Serialize(nuevoProducto, jsonOpt), System.Text.Encoding.UTF8, "application/json"));
+            Console.WriteLine(nuevo.IsSuccessStatusCode ? "Producto agregado." : "Error.");
+
+             Console.WriteLine("\nPresione una tecla para continuar...");
+            Console.ReadKey();   
+            //var nuevoJson = await http.GetStringAsync($"{baseUrl}/productos/porID/{nuevoProducto.Id}");
+            //var nuevoProductoAgregado = JsonSerializer.Deserialize<Producto>(nuevoJson, jsonOpt)!;
+            //Console.WriteLine($"El nuevo producto agregado es {nuevoProductoAgregado.Nombre} con un precio de {nuevoProductoAgregado.Precio:c} y un stock de {nuevoProductoAgregado.Stock}");
+            break;
+
+        case "6":
+            salir = true;
+            break;
+
+        default:
+            Console.WriteLine("Opción inválida.");
+            break;
+    }
+}
+
+class Producto
+{
     public int Id { get; set; }
     public string Nombre { get; set; } = null!;
     public decimal Precio { get; set; }
+    public int Cantidad { get; set; }
 }
 
 // Fin del ejemplo
--- TP/61473 - Lagoria García, Tomás Gustavo/tp5/servidor.cs
+++ TP/61473 - Lagoria García, Tomás Gustavo/tp5/servidor.cs
@@ -1,6 +1,7 @@
 #r "sdk:Microsoft.NET.Sdk.Web"
-#r "nuget: Microsoft.EntityFrameworkCore, 9.0.4"
-#r "nuget: Microsoft.EntityFrameworkCore.Sqlite, 9.0.4"
+#r "nuget: Microsoft.EntityFrameworkCore, 7.0.21"
+#r "nuget: Microsoft.EntityFrameworkCore.Sqlite, 7.0.21"
+#r "nuget: SQLitePCLRaw.bundle_e_sqlite3, 2.1.10"
 
 using System.Text.Json;                     
 using Microsoft.AspNetCore.Http;
@@ -9,6 +10,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Collections.Generic;
+SQLitePCL.Batteries_V2.Init();
 
 //  CONFIGURACIÓN
 var builder = WebApplication.CreateBuilder();
@@ -18,6 +22,44 @@ builder.Services.Configure<JsonOptions>(opt => opt.SerializerOptions.PropertyNam
 var app = builder.Build();
 
 app.MapGet("/productos", async (AppDb db) => await db.Productos.ToListAsync());
+
+app.MapGet("/productos/reponer", async (AppDb db) => await db.Productos.Where(p => p.Cantidad < 3).ToListAsync());
+
+app.MapGet("/productos/porID/{id}", async (int id, AppDb db) => await db.Productos.FindAsync(id) is Producto p ? Results.Ok(p) : Results.NotFound());
+
+app.MapPost("/productos/agregarNuevo", async (Producto p, AppDb db) =>
+{
+    db.Productos.Add(p);
+    await db.SaveChangesAsync();
+    return Results.Created($"/productos/{p.Id}", p);
+});
+app.MapPost("/productos/{id}/agregar/{cantidad}", async (int id, int cantidad, AppDb db) =>
+{
+    var producto = await db.Productos.FindAsync(id);
+    if (producto is null)
+    {
+        return Results.NotFound();
+    }
+    producto.Cantidad += cantidad;
+    await db.SaveChangesAsync();
+    return Results.Ok(producto);
+});
+app.MapPost("/productos/{id}/quitar/{cantidad}", async (int id, int cantidad, AppDb db) =>
+{
+    var producto = await db.Productos.FindAsync(id);
+    if (producto is null)
+    {
+        return Results.NotFound();
+    }
+    if (producto.Cantidad - cantidad < 0) 
+    {
+        return Results.BadRequest("Stock insuficiente.");
+    }
+         producto.Cantidad -= cantidad;
+        await db.SaveChangesAsync();
+        return Results.Ok(producto);
+
+    });
 
 var db = app.Services.GetRequiredService<AppDb>();
 db.Database.EnsureCreated(); // crear BD si no existe
@@ -34,8 +76,9 @@ class AppDb : DbContext {
     public DbSet<Producto> Productos => Set<Producto>();
 }
 
-class Producto{
+class Producto {
     public int Id { get; set; }
     public string Nombre { get; set; } = null!;
     public decimal Precio { get; set; }
+    public int Cantidad { get; set; }
 }
