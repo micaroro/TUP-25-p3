8546071762e91f55349e7bb405641872d11949a8
Alejandro Di Battista <alejandrodibattista@gmail.com>
2025-06-16 23:23:57 -0300

    .

--- servidor/Dto/ProductoDto.cs
+++
@@ -1,8 +0,0 @@
-namespace servidor.Dto;
-
-public class ProductoDto
-{
-    public int Id { get; set; }
-    public string Nombre { get; set; } = string.Empty;
-    public decimal Precio { get; set; }
-}
