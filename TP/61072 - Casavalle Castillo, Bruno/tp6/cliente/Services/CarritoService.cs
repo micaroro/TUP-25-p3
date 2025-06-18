fa2e5aa31eb572baa0168e8bdf0ef2b8fc9ce19f
BrunoLCC <136395583+BrunoLCC@users.noreply.github.com>
2025-06-18 09:15:43 -0300

    Correcciones, .NET, Arreglamos el Stock (#594)

--- TP/61072 - Casavalle Castillo, Bruno/tp6/cliente/Services/CarritoService.cs
+++ TP/61072 - Casavalle Castillo, Bruno/tp6/cliente/Services/CarritoService.cs
@@ -40,27 +40,65 @@ public class CarritoService
 
     public IReadOnlyList<ItemCarrito> ObtenerItems() => carrito;
 
-    public void AgregarProducto(Producto producto, int cantidad)
+    public async Task AgregarProducto(Producto producto, int cantidad)
     {
+        // Obtener stock actualizado
+        var productoActualizado = await ObtenerProductoPorId(producto.Id);
+        if (productoActualizado == null)
+            return;
+
         var existente = carrito.FirstOrDefault(i => i.ProductoId == producto.Id);
+        int cantidadEnCarrito = existente?.Cantidad ?? 0;
+        int stockDisponible = productoActualizado.Stock - cantidadEnCarrito;
+
+        if (stockDisponible <= 0)
+            return;
+
+        int cantidadAAgregar = Math.Min(cantidad, stockDisponible);
+
         if (existente != null)
         {
-            if (existente.Cantidad + cantidad <= producto.Stock)
-            {
-                existente.Cantidad += cantidad;
-            }
-            else
-            {
-                existente.Cantidad = producto.Stock;
-            }
+            existente.Cantidad += cantidadAAgregar;
         }
         else
         {
-            int cantidadAAgregar = Math.Min(cantidad, producto.Stock);
-            carrito.Add(new ItemCarrito(producto, cantidadAAgregar));
+            carrito.Add(new ItemCarrito(productoActualizado, cantidadAAgregar));
         }
 
         NotificarCambio();
+    }
+
+    public async Task IncrementarCantidad(int productoId)
+    {
+        var item = carrito.FirstOrDefault(i => i.ProductoId == productoId);
+        if (item != null)
+        {
+            var producto = await ObtenerProductoPorId(productoId);
+            if (producto != null)
+            {
+                int stockDisponible = producto.Stock - item.Cantidad;
+
+                if (stockDisponible > 0)
+                {
+                    item.Cantidad++;
+                    NotificarCambio();
+                }
+            }
+        }
+    }
+
+    public void DisminuirCantidad(int productoId)
+    {
+        var item = carrito.FirstOrDefault(i => i.ProductoId == productoId);
+        if (item != null)
+        {
+            item.Cantidad--;
+            if (item.Cantidad <= 0)
+            {
+                carrito.Remove(item);
+            }
+            NotificarCambio();
+        }
     }
 
     public void EliminarProducto(int productoId)
@@ -80,34 +118,6 @@ public class CarritoService
     }
 
     public decimal CalcularTotal() => carrito.Sum(i => i.Subtotal);
-
-    public async Task IncrementarCantidad(int productoId)
-    {
-        var item = carrito.FirstOrDefault(i => i.ProductoId == productoId);
-        if (item != null)
-        {
-            var producto = await ObtenerProductoPorId(productoId);
-            if (producto != null && item.Cantidad < producto.Stock)
-            {
-                item.Cantidad++;
-                NotificarCambio();
-            }
-        }
-    }
-
-    public void DisminuirCantidad(int productoId)
-    {
-        var item = carrito.FirstOrDefault(i => i.ProductoId == productoId);
-        if (item != null)
-        {
-            item.Cantidad--;
-            if (item.Cantidad <= 0)
-            {
-                carrito.Remove(item);
-            }
-            NotificarCambio();
-        }
-    }
 
     private void NotificarCambio() => OnChange?.Invoke();
 
