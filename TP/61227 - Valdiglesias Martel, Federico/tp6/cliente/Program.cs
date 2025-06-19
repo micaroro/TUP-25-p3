61461ca7977a813bb0d95c9238e59dbb1917665b
Federico Valdiglesias <78738426+FedericoVM@users.noreply.github.com>
2025-06-19 09:07:57 -0300

    61227 - Valdiglesias, Federico - TP6 - Corrección (#662)
    * Update Program.cs
    
    Se agregaron los links de las imagenes
    
    * Update ApiService.cs
    
    Corrección para la actualización de stock y vaciado de carrito
    
    * Update MainLayout.razor
    
    Se modifico el aspecto visual y se agregaron las funcionalidades a los botones
    
    * Update TarjetaProducto.razor
    
    Se modifico la tarjeta en lo visual y para mostrar el stock en tiempo real
    
    * Update Carrito.razor
    
    Se modifico el formato de los precios y a la hora de comprar o cancelar
    
    * Update ConfirmarCarrito.razor
    
    Corrección de errores
    
    * Update Home.razor
    
    Se realizo algunas correcciones para actualizar stock
    
    * Update Program.cs
    
    * Update TiendaDb.cs
    
    Se agrego nuevamente un archivo que se borro de manera accidental
    
    * Update tienda.db

--- TP/61227 - Valdiglesias Martel, Federico/tp6/cliente/Program.cs
+++ TP/61227 - Valdiglesias Martel, Federico/tp6/cliente/Program.cs
@@ -7,7 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { 
-    // Revisa que este sea el puerto de tu backend
     BaseAddress = new Uri("http://localhost:5184") 
 });
 
