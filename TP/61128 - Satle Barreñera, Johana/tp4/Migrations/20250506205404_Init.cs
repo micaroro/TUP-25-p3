d79cddad7cfbb8ba23577caf2bd6f8d78802bb85
Joosatle <barrenadajohana@gmail.com>
2025-06-19 10:45:21 -0300

    TP4 - 61128 - Johana Satle Barreñada (#636)
    * Update
    
    * Update Program.cs
    
    * Update tp4.csproj
    
    * Update Program.cs
    
    * Delete TP/61128 - Satle Barreñera, Johana/tp4/Program.cs
    
    * Delete TP/61128 - Satle Barreñera, Johana/tp4/tp4.csproj
    
    ---------
    
    Co-authored-by: Alejandro Di Battista <alejandrodibattista@gmail.com>

---
+++ TP/61128 - Satle Barreñera, Johana/tp4/Migrations/20250506205404_Init.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tp4.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Preguntas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Enunciado = table.Column<string>(type: "TEXT", nullable: false),
+                    A = table.Column<string>(type: "TEXT", nullable: false),
+                    B = table.Column<string>(type: "TEXT", nullable: false),
+                    C = table.Column<string>(type: "TEXT", nullable: false),
+                    Correcta = table.Column<char>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preguntas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resultados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Alumno = table.Column<string>(type: "TEXT", nullable: false),
+                    Correctas = table.Column<int>(type: "INTEGER", nullable: false),
+                    Total = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nota = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resultados", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Preguntas");
+
+            migrationBuilder.DropTable(
+                name: "Resultados");
+        }
+    }
+}
