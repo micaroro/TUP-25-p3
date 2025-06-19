dd86d99710522cf2b015d76bbb9e59a79a908676
Alex <matatitanes1@gmail.com>
2025-06-19 09:08:23 -0300

    61572 parrado navarrete, alex daniel (#652)
    * 61572-Parrado Navarrete,Alex Daniel
    
    Elimino todas las antiguas carpetas para realizar el codigo de 0
    
    * 61572
    
    Empiezo con la creacion de los modelos con su respectivo nombre
    
    * 61572
    
    Modifico los archivos en program.cs cambiandolos a anteriormente junto a los del csproj
    
    * 61572
    
    Creo la base de datos y los productos con sus imagenes
    
    * 61572
    
    Termino toda la parte de cliente y cambios en los archivos de la api en servidor para que funcione la base de datos y aparezcan los productos
    
    * 61572
    
    Cambio el nombre de la fuente para que los precios en vez de euros aparezcan en pesos

--- TP/61572 - Parrado Navarrete, Alex Daniel/tp6/Cliente/Models/Cliente.cs
+++
@@ -1,7 +0,0 @@
-namespace TiendaOnline.Cliente.Models;
-public class Cliente
-{
-    public string NombreCliente { get; set; }
-    public string ApellidoCliente { get; set; }
-    public string EmailCliente { get; set; }
-}
