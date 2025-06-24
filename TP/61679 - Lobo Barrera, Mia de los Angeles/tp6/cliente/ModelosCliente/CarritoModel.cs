8c1afb990f42d6aa2c2701cc19c6f96ba6a79b70
Mia <barreramia2005@gmail.com>
2025-06-24 00:25:59 -0300

    61679- TP6 (#730)

--- TP/61679 - Lobo Barrera, Mia de los Angeles/tp6/cliente/ModelosCliente/CarritoModel.cs
+++ TP/61679 - Lobo Barrera, Mia de los Angeles/tp6/cliente/ModelosCliente/CarritoModel.cs
@@ -4,7 +4,7 @@ namespace cliente.Modelos
 {
     public class CarritoModel
     {
-        public int Id { get; set; } // Necesario para saber el ID del carrito
-        public List<CarritoItem> Items { get; set; } = new(); // Necesario para inicializar un carrito nuevo
+        public int Id { get; set; }
+        public List<CarritoItem> Items { get; set; } = new();
     }
 }
